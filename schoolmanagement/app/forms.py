ad6d07ef6c6d9383f08ee4b3e8a5ac04f1e0825e
unknown <your email address>
2019-07-08 23:15:31 +0530

    schoolmanagement

---
+++ schoolmanagement/app/forms.py
@@ -0,0 +1,12 @@
+	
+from django import forms
+from models import Student
+
+
+class StudentModelForm(forms.ModelForm):
+	class Meta:
+		model=Student
+		fields ="__all__"
+
+
+
---
+++ schoolmanagement/app/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Student(models.Model):
+	name=models.CharField(max_length=220)
+	phone=models.CharField(max_length=220)
+	email=models.EmailField()
+	add=models.TextField()
+class Userprofile(User):
+	phone=models.CharField(max_length=250)
+	add=models.TextField()
+
+
